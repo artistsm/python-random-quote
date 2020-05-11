6dc343d16d4f8e554886fd01de31cdf5fd8e260a
artistsm <42282656+artistsm@users.noreply.github.com>
2020-05-10 21:44:08 -0400

    Add quote to file programatically

--- get-quote.py
+++ get-quote.py
@@ -2,14 +2,19 @@ import random
 def primary():
   #print("Keep it logically awesome.")
 
+  f = open("quotes.txt", "a")
+  f.write("Truth or Dare!")
+  f.close()
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
   last = len(quotes) -1
   rnd = random.randint(0, last)
+  rnd1 = random.randint(0, last)
 
-  print(quotes[rnd])
+  print(quotes[rnd], end = '')
+  print(quotes[rnd1])
 
 if __name__== "__main__":
   primary()
